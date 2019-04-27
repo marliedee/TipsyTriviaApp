bd2dea62d1f1fcfd0d5562913564218a8942fbe5
khaisoe <khaingsoe@pursuit.org>
2019-04-27 14:12:20 -0400

    added findviewbyids

--- app/src/main/java/com/android/tipsytrivia/CategoryPageActivity.java
+++ app/src/main/java/com/android/tipsytrivia/CategoryPageActivity.java
@@ -2,14 +2,24 @@ package com.android.tipsytrivia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CategoryPageActivity extends AppCompatActivity {
 
-
+    private TextView categoryTextView;
+    private ImageView filmImage;
+    private ImageView tvImage;
+    private ImageView celebrityImage;
+    private ImageView animalImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        categoryTextView = findViewById(R.id.category);
+        filmImage = findViewById(R.id.film);
+        tvImage = findViewById(R.id.tv);
+        celebrityImage = findViewById(R.id.celebrities);
+        animalImage = findViewById(R.id.animals);
     }
 }

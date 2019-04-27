ab70ba5339aee7918fb467d2f5942ff0af5e9e89
Joseph Salcedo <joseph.salcedo16@qmail.cuny.edu>
2019-04-27 19:51:39 -0400

    Fixing logic to determine right answer

---
+++ app/src/main/java/com/android/tipsytrivia/WrongResultActivity.java
@@ -0,0 +1,46 @@
+package com.android.tipsytrivia;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.felipecsl.gifimageview.library.GifImageView;
+
+public class WrongResultActivity extends AppCompatActivity {
+    private GifImageView gifImageView;
+    private TextView textView;
+
+    private Handler handler;
+    private Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrong_result);
+        textView = findViewById(R.id.wrong_text_results);
+
+        gifImageView = findViewById(R.id.drink_ImageView);
+        Glide.with(this).load(R.raw.cheers).into(gifImageView);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), CategoryPageActivity.class));
+                finish();
+            }
+
+        };
+        handler = new Handler();
+        handler.postDelayed(runnable, 3000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (handler != null && runnable != null) ;
+        handler.removeCallbacks(runnable);
+    }
+}

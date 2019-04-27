408d3a6c28f521a2f548a56f5b9849a68d5f3154
marliedee <marlyd364@gmail.com>
2019-04-27 15:01:14 -0400

    begun radio button logic

--- app/src/main/java/com/android/tipsytrivia/QuestionsPageActivity.java
+++ app/src/main/java/com/android/tipsytrivia/QuestionsPageActivity.java
@@ -2,13 +2,40 @@ package com.android.tipsytrivia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class QuestionsPageActivity extends AppCompatActivity {
-
+ private String [] incorrectChoiceArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_page);
 
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.textview_multiple_choice_one:
+                if (checked)
+                    // Choice One
+                    break;
+            case R.id.textview_multiple_choice_two:
+                if (checked)
+                    // Choice Two
+                    break;
+            case R.id.textview_multiple_choice_three:
+                if (checked)
+                    // Choice Three
+                    break;
+            case R.id.textview_multiple_choice_four:
+                if (checked)
+                    // Choice Four
+                    break;
+        }
     }
 }
